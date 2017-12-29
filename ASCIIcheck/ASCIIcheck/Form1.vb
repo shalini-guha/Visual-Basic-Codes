5637ff791fb153f418ccb93085e5e8ffe6c44135
Shalini Guha <shaliniguha2@gmail.com>
2017-12-29 12:55:03 +0530

    Add files via upload

---
+++ ASCIIcheck/ASCIIcheck/Form1.vb
@@ -0,0 +1,15 @@
+﻿Public Class Form1
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        Dim x, y As String
+        x = TextBox1.Text
+        y = AscW(x)
+        TextBox2.Text = Val(y)
+
+    End Sub
+
+    Private Sub Button2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button2.Click
+        TextBox1.Text = ""
+        TextBox2.Text = ""
+    End Sub
+End Class
