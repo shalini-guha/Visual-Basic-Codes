6d316b16022d52233d3c7fcf538ae5bbbfdf235b
Shalini Guha <shaliniguha2@gmail.com>
2017-12-29 13:06:20 +0530

    Add files via upload

---
+++ Factorial/Factorial/Form1.vb
@@ -0,0 +1,16 @@
+﻿Public Class Form1
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        Dim j, valu, fact As Double
+        valu = TextBox1.Text
+        j = 1
+        fact = 1
+        While (j <= valu)
+            fact = fact * j
+            j = j + 1
+        End While
+        Label1.Text = fact
+    End Sub
+End Class
+
+
---
+++ largest/largest/Form1.vb
@@ -0,0 +1,19 @@
+﻿Public Class Form1
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        Dim a, b, c, lar As Integer
+        a = TextBox1.Text
+        b = TextBox2.Text
+        c = TextBox3.Text
+        lar = a
+        If b > a Then
+            If b > c Then
+                lar = b
+            Else
+                lar = c
+
+            End If
+        End If
+        Label2.Text = lar
+    End Sub
+End Class
